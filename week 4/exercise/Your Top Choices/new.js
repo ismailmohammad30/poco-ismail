2a99bd10a6eb3c17bc490b0124fcb6a71b65287c
ismailmohammad <ismail.mohammad@powercoders.org>
2020-10-03 18:19:56 +0100

    new update

---
+++ week 4/exercise/Your Top Choices/new.js
@@ -0,0 +1,8 @@
+
+ var colors = ["[my #1 choice is black]",
+ "      [my #2 choice is brown]",
+ "        [my #3 choice is red]"];
+ document.write(colors[0]);
+ document.write(colors[1]);
+ document.write(colors[2]);
+
