fb565f4bb97d220f520493a715fa47f533d987e1
ismailmohammad <ismail.mohammad@powercoders.org>
2020-10-04 14:03:14 +0100

    done from hello world exe

---
+++ week 4/exercises js/helloWorld/helloWorld.js
@@ -0,0 +1,12 @@
+function sayHello(language) {
+	var arrHellos = {
+		
+		"Dutch" : "Hallo wereld",
+		"English" : "Hello world",
+		"French" : "Bonjour monde",
+		"German" : "Hallo Welt",
+        "Spanish" : "Hola mundo",
+        "Arabic"   :"مرحبا عالمنا"
+	};
+	alert(arrHellos[language]);
+}
