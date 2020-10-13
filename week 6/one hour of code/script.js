ad3d74ac74ff8e93513af6b0cced12411a105e91
ismailmohammad <ismail.mohammad@powercoders.org>
2020-10-13 14:00:53 +0100

    add one hour of code

--- week 6/one hour of code/script.js
+++ week 6/one hour of code/script.js
@@ -1,4 +1,4 @@
-let colorCode= document.querySelector("code");
+let colorCode= document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("colors");
@@ -6,7 +6,7 @@ let body = document.getElementById("colors");
 function changeColor (){
   body.style.background=
   "linear-gradient(to right,"+ color1.value + ","+ color2.value + ")";
-  
+
 }
 color1.addEventListener("input",changeColor);
 color2.addEventListener("input",changeColor);
